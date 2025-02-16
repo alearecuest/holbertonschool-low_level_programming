d5bf9ed7b61a483407f4cc622d2bf82ddd23a5d2
Alejandro <10653@holbertonstudents.com>
2025-02-16 20:09:13 +0000

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Description: Prints the sizes of various data types on the computer
- * it is compiled and run.
+ * it is compiled and run on.
  *
- * Returno: Always 0 (Success)
+ * Return: 0 if the program executes successfully.
  */
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+       	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+        printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-
+	
 	return (0);
 }
