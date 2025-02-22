16c8de2e24d1ccc2a9f23a863d4b8f4a7f4edc58
Alejandro <10653@holbertonstudents.com>
2025-02-22 21:44:18 +0000

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,23 +14,20 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 				product = i * j;
-				if (j > 0)
+				if (j != 0)
 				{
-					_putchar(',');
-					_putchar(' ');
 					if (product < 10)
 					{
 						_putchar(',');
 					}
+					_putchar(' ');
 				}
-				if (product >= 10)
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+
+				if (j != 9)
 				{
-					_putchar((product / 10) + '0');
-					_putchar((product % 10) + '0');
-				}
-				else
-				{
-					_putchar(product + '0');
+					_putchar(',');
 				}
 		}
 		_putchar('\n');
