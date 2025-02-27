9da8766e7f82cc373f1ec9c332c7d4ee403f4783
Alejandro <10653@holbertonstudents.com>
2025-02-27 17:05:37 +0000

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - inverse a string of characters.
+ * @s: string of characters.
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int leng = 0;
+	int i;
+	char temp;
+
+	while (s[leng] != ('\n'))
+	{
+		leng++;
+	}
+
+	for (i = 0; i < leng / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[leng - 1 - i];
+		s[leng - 1 - i] = temp;
+	}
+}
