a38cab86b03caa3e0a979e9267cd722d75e8085f
Alejandro <10653@holbertonstudents.com>
2025-02-28 21:19:46 +0000

    1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -13,6 +13,11 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0;
 	int j = 0;
 
+	while (dest[i] != ('\0'))
+	{
+		i++;
+	}
+
 	while (dest[i] != ('\0') && j < n)
 	{
 		dest[i] = src[j];
