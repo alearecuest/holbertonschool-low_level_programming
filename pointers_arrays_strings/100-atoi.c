341f2a0d0f570922e356286d15df963b0a6c1eb0
Alejandro <10653@holbertonstudents.com>
2025-03-01 04:57:43 +0000

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,36 +2,36 @@
 #include <limits.h>
 
 /**
- * _atoi - converse a chain in a integer number.
- * @s: chain of characters.
+ * _atoi - transforms a strings in an integer number with
+ * drive of signs and errors.
+ * @s: La cadena de entrada.
  *
- * Return: the integer number converted in a chain
+ * Return: integer number transforms, if no numbers be 0
  */
 int _atoi(char *s)
 {
-	int sign = 1;
+	int sign = 1, num_started = 0;
 	int result = 0;
-	int i = 0;
 
-	while (s[i] != ('\0') && (s[i] < '0' || s[i] > '9'))
+	while (*s)
 	{
-		if (s[i] == '-')
-		{
+		if (*s == '-')
 			sign *= -1;
-		}
-		i++;
-	}
+		else if (*s == '+')
+			sign *= 1;
 
-	while (s[i] != ('\0') && (s[i] >= '0' && s[i] <= '9'))
-	{
-		if (result < INT_MAX / 10 || (result == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
+		else if (*s >= '0' && *s <= '9')
 		{
-			return (sign == 1 ? INT_MAX : INT_MIN);
+			num_started = 1;
+			if (result > (INT_MAX / 10) || (result == (INT_MAX / 10) && (*s - '0') > (INT_MAX % 10)))
+				return (sign == 1 ? INT_MAX : INT_MIN);
+
+			result = result * 10 + (*s - '0');
 		}
+		else if (num_started)
+			break;
 
-		result = (result * 10) + (s[i] - '0');
-		i++;
+		s++;
 	}
-
-	return (result * sign);
+	return (num_started ? result * sign : 0);
 }
