ff3ab4c7adbce12389e83edd8daee32bd6b9cdb2
Alejandro <10653@holbertonstudents.com>
2025-03-05 13:26:51 +0000

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,32 +2,32 @@
 #include <math.h>
 
 /**
- * _sqrt_recursion - returns the natural square root of a number.
- * @n: the number to calculate the square root of
+ * _sqrt_helper - helper function to find the square root.
+ * @n: the number to calculate the square root.
+ * @x: the current value to test as the square root.
  *
- * Return: the natural square root of n, or -1 if n does not have a natural square root
+ * Return: the natural square root of n, or -1 if n
+ * does not have a natural square root
  */
-int _sqrt_recursion(int n)
+int _sqrt_helper(int n, int x)
 {
-	int sqrt(int n, int i);
-
-	if (n < 0)
+	if (x * x > n)
 		return (-1);
-	return (sqrt(n, 1));
+	if (x * x == n)
+		return (x);
+	return (_sqrt_helper(n, x + 1));
 }
 
 /**
- * sqrt - helper function to find the square root.
- * @n: the number to calculate the square root of
- * @i: the current value to test as the square root
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: the number to calculate the square root.
  *
- * Return: the natural square root of n, or -1 if n does not have a natural square root
+ * Return: the natural square root of n, or -1 if n
+ * does not have a natural square root
  */
-int sqrt(int n, int i)
+int _sqrt_recursion(int n)
 {
-	if (i * i > n)
+	if (n < 0)
 		return (-1);
-	if (i * i == n)
-		return (i);
-	return (sqrt(n, i + 1));
+	return (_sqrt_helper(n, 0));
 }
