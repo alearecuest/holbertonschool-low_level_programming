351e212de1accc4a3b1e65a6de82ed82b02a3e40
Alejandro <10653@holbertonstudents.com>
2025-03-12 01:37:27 +0000

    0-malloc_checked.c

---
+++ more_malloc_free/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void *malloc_checked(unsigned int b);
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+char *create_array(unsigned int size, char c);
+int is_prime_number(int n);
+int _sqrt_recursion(int n);
+int _pow_recursion(int x, int y);
+int factorial(int n);
+int _strlen_recursion(char *s);
+void _print_rev_recursion(char *s);
+void _puts_recursion(char *s);
+int _putchar(char c);
+
+#endif /* MAIN_H */
