398b6644069c8485e7631014294203ffaf46cb42
Alejandro <10653@holbertonstudents.com>
2025-03-14 18:12:06 +0000

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,7 +12,8 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
-	unsigned int total_size;
+	unsigned int total_size, i;
+	char *char_ptr;
 
 	if (nmemb == 0 || size == 0)
 	{
@@ -28,7 +29,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 
-	memset(ptr, 0, total_size);
+	char_ptr = (char *)ptr;
+
+	for (i = 0; i < total_size; i++)
+	{
+		char_ptr[i] = 0;
+	}
 
 	return (ptr);
 }
