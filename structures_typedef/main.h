011865ab9bd87c911c93cdaa5587f0a6d9076dfb
Alejandro <10653@holbertonstudents.com>
2025-03-15 21:33:21 +0000

    1-init_dog.c

---
+++ structures_typedef/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *malloc_checked(unsigned int b);
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+char *create_array(unsigned int size, char c);
+int is_prime_number(int n);
+int _sqrt_recursion(int n);
+int _pow_recursion(int x, int y);
+int factorial(int n);
+int _strlen_recursion(char *s);
+void _print_rev_recursion(char *s);
+void _puts_recursion(char *s);
+int _putchar(char c);
+
+#endif /* MAIN_H */
