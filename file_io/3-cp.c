cbc8281d17986e4f212cebbbe5032b10af7fe08d
alearecuest <126989105+alearecuest@users.noreply.github.com>
2025-04-06 20:50:40 -0300

    3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,8 +13,8 @@
  */
 void error_file(int exit_code, const char *message, const char *filename)
 {
-	dprintf(STDERR_FILENO, "%s %s\n", message, filename);
-	exit(exit_code);
+    dprintf(STDERR_FILENO, "%s %s\n", message, filename);
+    exit(exit_code);
 }
 
 /**
@@ -25,12 +25,80 @@ void error_file(int exit_code, const char *message, const char *filename)
  */
 void error_fd(int exit_code, const char *message, int fd)
 {
-	dprintf(STDERR_FILENO, "%s %d\n", message, fd);
-	exit(exit_code);
+    dprintf(STDERR_FILENO, "%s %d\n", message, fd);
+    exit(exit_code);
 }
 
 /**
- * main - copies content from one file to another
+ * open_files - opens source and destination files
+ * @file_from: source filename
+ * @file_to: destination filename
+ * @fd_from: pointer to source file descriptor
+ * @fd_to: pointer to destination file descriptor
+ */
+void open_files(char *file_from, char *file_to, int *fd_from, int *fd_to)
+{
+    *fd_from = open(file_from, O_RDONLY);
+    if (*fd_from == -1)
+        error_file(98, "Error: Can't read from file", file_from);
+
+    *fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+    if (*fd_to == -1)
+    {
+        close(*fd_from);
+        error_file(99, "Error: Can't write to", file_to);
+    }
+}
+
+/**
+ * copy_data - copies data from source to destination
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ * @file_from: source filename (for error messages)
+ * @file_to: destination filename (for error messages)
+ */
+void copy_data(int fd_from, int fd_to, char *file_from, char *file_to)
+{
+    int rd, wr;
+    char buffer[BUFFER_SIZE];
+
+    rd = read(fd_from, buffer, BUFFER_SIZE);
+    while (rd > 0)
+    {
+        wr = write(fd_to, buffer, rd);
+        if (wr == -1 || wr != rd)
+        {
+            close(fd_from);
+            close(fd_to);
+            error_file(99, "Error: Can't write to", file_to);
+        }
+        rd = read(fd_from, buffer, BUFFER_SIZE);
+    }
+
+    if (rd == -1)
+    {
+        close(fd_from);
+        close(fd_to);
+        error_file(98, "Error: Can't read from file", file_from);
+    }
+}
+
+/**
+ * close_files - closes file descriptors
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ */
+void close_files(int fd_from, int fd_to)
+{
+    if (close(fd_from) == -1)
+        error_fd(100, "Error: Can't close fd", fd_from);
+
+    if (close(fd_to) == -1)
+        error_fd(100, "Error: Can't close fd", fd_to);
+}
+
+/**
+ * main - copies content of a file to another file
  * @argc: argument count
  * @argv: argument vector
  *
@@ -38,42 +106,17 @@ void error_fd(int exit_code, const char *message, int fd)
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, rd, wr;
-	char buffer[BUFFER_SIZE];
+    int fd_from, fd_to;
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+    if (argc != 3)
+    {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		error_file(98, "Error: Can't read from file", argv[1]);
+    open_files(argv[1], argv[2], &fd_from, &fd_to);
+    copy_data(fd_from, fd_to, argv[1], argv[2]);
+    close_files(fd_from, fd_to);
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-		error_file(99, "Error: Can't write to", argv[2]);
-
-	while (1)
-	{
-		rd = read(fd_from, buffer, BUFFER_SIZE);
-		if (rd == -1)
-			error_file(98, "Error: Can't read from file", argv[1]);
-
-		if (rd == 0)
-			break;
-
-		wr = write(fd_to, buffer, rd);
-		if (wr == -1 || wr != rd)
-			error_file(99, "Error: Can't write to", argv[2]);
-	}
-
-	if (close(fd_from) == -1)
-		error_fd(100, "Error: Can't close fd", fd_from);
-
-	if (close(fd_to) == -1)
-		error_fd(100, "Error: Can't close fd", fd_to);
-
-	return (0);
+    return (0);
 }
